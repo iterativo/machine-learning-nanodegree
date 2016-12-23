9908e0c61999bb6572876518014b9c4639e63ce7
Rafael Torres <iterativo@users.noreply.github.com>
2016-12-23 10:56:21 -0600

    Update agent state

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -56,7 +56,7 @@ class LearningAgent(Agent):
         ## TO DO ##
         ###########
         # Set 'state' as a tuple of relevant data for the agent        
-        state = None
+        state = (inputs["light"], inputs["oncoming"], inputs["left"], inputs["right"], deadline)
 
         return state
 
@@ -160,6 +160,7 @@ def run():
     # Flags:
     #   enforce_deadline - set to True to enforce a deadline metric
     env.set_primary_agent(agent, enforce_deadline=True)
+    # env.set_primary_agent(agent)
 
     ##############
     # Create the simulation
@@ -169,6 +170,7 @@ def run():
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
     sim = Simulator(env, update_delay=0.01, log_metrics=True, display=False)
+    # sim = Simulator(env)
     
     ##############
     # Run the simulator
@@ -176,6 +178,7 @@ def run():
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
     sim.run(n_test=10)
+    # sim.run()
 
 
 if __name__ == '__main__':
