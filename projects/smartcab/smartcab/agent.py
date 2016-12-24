0a644158bceb565e3917564a2fea22f5d20f9ee7
Rafael Torres <iterativo@users.noreply.github.com>
2016-12-24 09:03:00 -0600

    Optimize

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -46,10 +46,7 @@ class LearningAgent(Agent):
             self.epsilon = 0
             self.alpha = 0
         else:
-            self.epsilon = self.epsilon - 0.05
-
-        if self.epsilon <= 0:
-            self.epsilon = 0
+            self.epsilon = 1/float(self.trial**2)
 
         return None
 
@@ -190,7 +187,7 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, log_metrics=True, display=False)
+    sim = Simulator(env, update_delay=0.01, log_metrics=True, display=False, optimized=True)
     # sim = Simulator(env)
     
     ##############
