6b201eaa95a94f6a0cda234845ee628420c23810
Rafael Torres <iterativo@users.noreply.github.com>
2016-12-27 10:39:58 -0600

    Make corrections to smartcab project per review

--- projects/smartcab/smartcab/agent.py
+++ projects/smartcab/smartcab/agent.py
@@ -98,7 +98,7 @@ class LearningAgent(Agent):
         # When learning, check if the 'state' is not in the Q-table
         # If it is not, create a new dictionary for that state
         #   Then, for each action available, set the initial Q-value to 0.0
-        if state not in self.Q.keys():        
+        if (self.learning == True) and (state not in self.Q.keys()):        
             state_values = dict()
             for v in self.valid_actions:
                 state_values[v] = 0.0
